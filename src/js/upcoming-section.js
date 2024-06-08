21d35da8f91f51062e9ba2d882a29695caebdca3
SimaMNL <simamarinel2@gmail.com>
2024-06-08 21:00:58 +0200

    nu mai pot mor lent

--- src/js/upcoming-section.js
+++ src/js/upcoming-section.js
@@ -3,6 +3,7 @@ const apiKey = 'd76d8df6a2b0d7e69c80b59c6dadfabe';
 const apiUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US-EN&page=1`;
 const apiUrlGenre = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US-EN&page=1`;
 const BASE_URL = 'https://api.themoviedb.org/3/';
+const imagePath = 'https://image.tmdb.org/t/p/w1280/';
 fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
   .then(response => response.json())
   .then(response => console.log(response))
@@ -14,13 +15,14 @@ async function getMovieOfTheWeek() {
     const movieGenre = await fetch(apiUrlGenre);
     const data = await response.json();
     const movie = data.results[0];
+    const imagePath = 'https://image.tmdb.org/t/p/w1280/';
 
     document.getElementById('movie-title').innerText = movie.title;
     // document.getElementById('genre').innerText = movieGenre.id;
     document.getElementById('voteRating').innerText = movie.vote_average;
     document.getElementById('voteCount').innerText = movie.vote_count;
     document.getElementById('release_date').innerText = movie.release_date;
-    document.getElementById('moviePoster').innerText = movie.poster_path;
+    document.getElementById('moviePoster').innerText = imagePath.poster_path;
     document.getElementById('movie-overview').innerText = movie.overview;
     document.getElementById('movie-rating').innerText = `${movie.vote_average}`;
   } catch (error) {
