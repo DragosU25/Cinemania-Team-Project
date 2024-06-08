fe4de9736a7362bac13bec4883aacc073bff2b62
SimaMNL <simamarinel2@gmail.com>
2024-06-08 20:28:47 +0200

    efectiv mor

--- src/js/upcoming-section.js
+++ src/js/upcoming-section.js
@@ -2,7 +2,7 @@ const options = { method: 'GET', headers: { accept: 'application/json' } };
 const apiKey = 'd76d8df6a2b0d7e69c80b59c6dadfabe';
 const apiUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US-EN&page=1`;
 const apiUrlGenre = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US-EN&page=1`;
-
+const BASE_URL = 'https://api.themoviedb.org/3/';
 fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
   .then(response => response.json())
   .then(response => console.log(response))
@@ -16,7 +16,7 @@ async function getMovieOfTheWeek() {
     const movie = data.results[0];
 
     document.getElementById('movie-title').innerText = movie.title;
-    document.getElementById('genre').innerText = movieGenre.id;
+    // document.getElementById('genre').innerText = movieGenre.id;
     document.getElementById('voteRating').innerText = movie.vote_average;
     document.getElementById('voteCount').innerText = movie.vote_count;
     document.getElementById('release_date').innerText = movie.release_date;
@@ -26,11 +26,6 @@ async function getMovieOfTheWeek() {
   } catch (error) {
     console.error('Error fetching movie details:', error);
   }
-  var g = '';
-  for (i in genres) {
-    g += genres[i].name;
-  }
-  document.getElementById('genres').textContent = genres[i].name;
 }
 
 getMovieOfTheWeek();
@@ -50,3 +45,9 @@ getMovieOfTheWeek();
 //     throw error;
 //   }
 // };
+
+// e.preventDefault();
+// if (e.target.id === 'more-details') {
+//   let html = ;
+//   movieDetalisContainer.innerHTML = html;
+// }
