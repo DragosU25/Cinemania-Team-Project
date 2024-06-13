90e3e4fd340da51609c93e4a8ca82781ee4cd40b
Cristina-Tataru <151068609+Cristina-Tataru@users.noreply.github.com>
2024-06-13 14:01:20 +0300

    commit

---
+++ src/js/api-key.js
@@ -0,0 +1,23 @@
+const KEY = 'b388718d0206c62e77e14bdbca5dc2d7';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMG_BASE_URL = `https://image.tmdb.org/t/p`;
+const UPCOMING_URL = `${BASE_URL}/movie/upcoming`;
+const TREND_DF_URL = `${BASE_URL}/trending/movie/day`;
+const TREND_URL = `${BASE_URL}/trending/movie/week`;
+const VIDEO_URL = `${BASE_URL}/movie`;
+const SEARCH_URL = `${BASE_URL}/search/movie`
+const IMG_W400 = `/w400`;
+const IMAGE_URL_W500 = 'https://image.tmdb.org/t/p/w500';
+
+export {
+  KEY,
+  BASE_URL,
+  IMG_BASE_URL,
+  IMG_W400,
+  UPCOMING_URL,
+  TREND_DF_URL,
+  TREND_URL,
+  IMAGE_URL_W500,
+  VIDEO_URL,
+  SEARCH_URL,
+};
--- src/sass/api-key.js
+++
@@ -1,1 +0,0 @@
-const KEY= 'b388718d0206c62e77e14bdbca5dc2d7';
