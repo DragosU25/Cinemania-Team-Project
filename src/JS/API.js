306a4168eb5b35b8f09cbe5f4cf2d040dfb334d3
DragosU25 <149568004+DragosU25@users.noreply.github.com>
2024-06-16 22:32:55 +0200

    am schimbat sursele link urilor si imaginilor

--- src/JS/API.js
+++ src/JS/API.js
@@ -1,19 +1,13 @@
-import axios from 'axios';
+import axios from '../../node_modules/axios';
 
-const URL = "https://api.themoviedb.org/3/trending/all/day?language=en-US";
-const KEY = "6172d89f6dff35bbab361edfaaecc13a";
+const URL = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
+const KEY = '6172d89f6dff35bbab361edfaaecc13a';
 
 export async function getUser() {
-    try {
-      const respond = await axios.get(`${URL}&api_key=${KEY}`);
-     return respond.data
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const respond = await axios.get(`${URL}&api_key=${KEY}`);
+    return respond.data;
+  } catch (error) {
+    console.error(error);
   }
-
-
-
-
-  
- 
+}
